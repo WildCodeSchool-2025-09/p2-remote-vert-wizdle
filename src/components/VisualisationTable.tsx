8fe75f8e34291b2a0c3e5a49fd0c956308451a3e
leabpinheiro <leamelanie1998@hotmail.fr>
2025-11-13 11:18:51 +0100

    creation du calendrier de personnages

--- src/components/VisualisationTable.tsx
+++ src/components/VisualisationTable.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import "../styles/VisualisationTable.css";
+import calendarData from "../data/calendar.json";
 
 interface Personnage {
+	id: string;
 	nom: string;
 	espece: string;
 	genre: string;
@@ -14,94 +16,121 @@ interface Personnage {
 function VisualisationTable() {
 	const apiUrl = import.meta.env.VITE_API_URL;
 	const [allData, setAllData] = useState<Personnage[]>([]);
+	const [tableauTry, setTableauTry] = useState<Personnage[]>([]);
+	const today = new Date().toISOString().split("T")[0];
+	const calendar: Record<string, string> = calendarData;
+	const todayId = calendar[today];
+	const todayPerso = allData.find((personnage) => personnage.id === todayId);
 
 	useEffect(() => {
 		fetch(apiUrl)
 			.then((res) => res.json())
-			.then((data) => setAllData(data));
+			.then((data) => {
+				setAllData(data);
+			});
 	}, []);
 
 	if (allData.length === 0) {
 		return <p>Chargement des données...</p>;
 	}
 
-	const random = allData[Math.floor(allData.length * Math.random())];
-	const harryPotter = allData.find(
-		(personnage) => personnage.nom === "Harry Potter",
-	);
+	function pushRandom() {
+		if (allData.length === 0) return;
+		const random = allData[Math.floor(Math.random() * allData.length)];
+		setTableauTry((prev) => [random, ...prev]);
+	}
 
 	return (
 		<>
-			<table id="table-game">
-				<thead>
-					<tr>
-						<th>Nom</th>
-						<th>Espèce</th>
-						<th>Genre</th>
-						<th>Ascendance</th>
-						<th>Maison</th>
-						<th>En vie?</th>
-						<th>Cheveux</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td
-							className={
-								random.nom === harryPotter?.nom ? "box-true" : "box-false"
-							}
-						>
-							{harryPotter?.nom}
-						</td>
-						<td
-							className={
-								random.espece === harryPotter?.espece ? "box-true" : "box-false"
-							}
-						>
-							{harryPotter?.espece}
-						</td>
-						<td
-							className={
-								random.genre === harryPotter?.genre ? "box-true" : "box-false"
-							}
-						>
-							{harryPotter?.genre}
-						</td>
-						<td
-							className={
-								random.ascendance === harryPotter?.ascendance
-									? "box-true"
-									: "box-false"
-							}
-						>
-							{harryPotter?.ascendance}
-						</td>
-						<td
-							className={
-								random.maison === harryPotter?.maison ? "box-true" : "box-false"
-							}
-						>
-							{harryPotter?.maison}
-						</td>
-						<td
-							className={
-								random.vivant === harryPotter?.vivant ? "box-true" : "box-false"
-							}
-						>
-							{harryPotter?.vivant ? "oui" : "non"}
-						</td>
-						<td
-							className={
-								random.couleur_cheveux === harryPotter?.couleur_cheveux
-									? "box-true"
-									: "box-false"
-							}
-						>
-							{harryPotter?.couleur_cheveux}
-						</td>
-					</tr>
-				</tbody>
-			</table>
+			<button type="button" onClick={pushRandom}>
+				Ajoute un perso aléatoire
+			</button>
+			<div className="table-container">
+				<table id="table-game">
+					<thead>
+						<tr>
+							<th>Nom</th>
+							<th>Espèce</th>
+							<th>Genre</th>
+							<th>Ascendance</th>
+							<th>Maison</th>
+							<th>En vie?</th>
+							<th>Cheveux</th>
+						</tr>
+					</thead>
+					<tbody>
+						{tableauTry.length > 0 &&
+							tableauTry.map((personnage) => (
+								<tr key={personnage.id}>
+									<td
+										className={
+											todayPerso?.nom === personnage?.nom
+												? "box-true"
+												: "box-false"
+										}
+									>
+										<div className="cell">{personnage?.nom}</div>
+									</td>
+									<td
+										className={
+											todayPerso?.espece === personnage?.espece
+												? "box-true"
+												: "box-false"
+										}
+									>
+										{personnage?.espece}
+									</td>
+									<td
+										className={
+											todayPerso?.genre === personnage?.genre
+												? "box-true"
+												: "box-false"
+										}
+									>
+										{personnage?.genre}
+									</td>
+									<td
+										className={
+											todayPerso?.ascendance === personnage?.ascendance
+												? "box-true"
+												: "box-false"
+										}
+									>
+										{personnage?.ascendance}
+									</td>
+									<td
+										className={
+											todayPerso?.maison === personnage?.maison
+												? "box-true"
+												: "box-false"
+										}
+									>
+										{personnage?.maison}
+									</td>
+									<td
+										className={
+											todayPerso?.vivant === personnage?.vivant
+												? "box-true"
+												: "box-false"
+										}
+									>
+										{personnage?.vivant ? "oui" : "non"}
+									</td>
+									<td
+										className={
+											todayPerso?.couleur_cheveux ===
+											personnage?.couleur_cheveux
+												? "box-true"
+												: "box-false"
+										}
+									>
+										{personnage?.couleur_cheveux}
+									</td>
+								</tr>
+							))}
+					</tbody>
+				</table>
+			</div>
 		</>
 	);
 }
