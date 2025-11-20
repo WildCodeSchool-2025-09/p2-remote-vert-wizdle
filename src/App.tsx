b09c788e3db3ef3a4b0b7f8e98b4f6696e84ec3d
Thomas Rieu <rieu.thomas@hotmail.com>
2025-11-20 16:31:52 +0100

    addition of audio file in App.tsx + css part

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,25 @@ import Header from "./components/Header";
 function App() {
 	return (
 		<div className="background">
+			<aside>
+				<figure>
+					<audio controls>
+						<source
+							src="./src/assets/media/harry-potter-generique.mp3"
+							type="audio/mpeg"
+						/>
+						<track
+							src="./src/assets/media/harry-potter-generique.vtt"
+							kind="captions"
+							srcLang="fr"
+							label="Transcription audio"
+						/>
+						Votre navigateur ne supporte pas la balise audio.
+					</audio>
+					<figcaption>Génerique Harry Potter</figcaption>
+				</figure>
+			</aside>
 			<Header />
-
 			<main>
 				<Outlet />
 			</main>
